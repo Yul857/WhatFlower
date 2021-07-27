62d11ffafa25a0d5c8db611cd837efdb888da50e
Lin Yu-Ming <Yul857@ucsd.edu>
2021-07-26 19:46:50 -0700

    add UIPickerViewController

---
+++ WhatFlower/WhatFlower/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Yu Ming Lin on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
+        
+        imageView.image = userPickedImage as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+}
+
